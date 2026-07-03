769c7b9d7b929594a97d5d074399b24fde05e39f
Narasimha <nsojasmedialabs@gmail.com>
2026-07-03 07:53:21 +0530

    fix: ultimate layout bypass

--- apps/cms/src/app/(payload)/layout.tsx
+++ apps/cms/src/app/(payload)/layout.tsx
@@ -1,6 +1,7 @@
+// @ts-nocheck
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-// @ts-nocheck
+
 import config from '@payload-config'
 import '@payloadcms/next/css'
 import type { ServerFunctionClient } from 'payload'
